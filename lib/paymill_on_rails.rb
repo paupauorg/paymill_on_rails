32fca7bdda334b078a94ec731a6ff70065df0d6f
poloniculmov <github@poloniculmov.com>
2013-11-21 14:29:19 +0200

    Added keys initializer

--- lib/paymill_on_rails.rb
+++ lib/paymill_on_rails.rb
@@ -1,4 +1,17 @@
-require "paymill_on_rails/engine"
+require 'paymill_on_rails/engine'
 
 module PaymillOnRails
+
+  mattr_accessor :paymill_private_key
+  @@paymill_private_key = nil
+
+  mattr_accessor :paymill_public_key
+  @@paymill_public_key = nil
+
+  def self.setup
+    yield self
+
+    Paymill.api_key = paymill_private_key
+  end
+
 end
